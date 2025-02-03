5db800dd0497bbe8b9f25c8ba62136bb47668bac
sanketmishra009 <sanketmishra009@gmail.com>
2025-02-03 23:14:21 +0530

    practice.

--- basics/js/main.js
+++ basics/js/main.js
@@ -16,17 +16,39 @@
 
 // gsap.from(".logo", { duration: 2, opacity: 0, scale: 0.3, ease: "back" });
 
-gsap.to(".square", {
+gsap.from(".square", {
   scale: 0.1,
-  duration: 1.5,
+  duration: 0.5,
   y: 40,
   ease: "power1.inOut",
+  borderRadius: "50%",
   repeat: -1,
   yoyo: true,
   stagger: {
-    grid: [5, 10],
+    grid: [8, 10],
     from: "center",
     // axis: "x",
     amount: 1.5,
   },
 });
+
+let tl = gsap.timeline({});
+
+tl.from(".name", {
+  y: -40,
+  duration: 1,
+  ease: "power4.inOut",
+  opacity: 0,
+});
+
+tl.from(
+  ".nav-link",
+  {
+    duration: 1,
+    y: -40,
+    ease: "power4.Out",
+    opacity: 0,
+    stagger: 0.8,
+  }
+  // "-=0.5"
+);
